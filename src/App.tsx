a1a34d1a89edb465a847922e54df416877643966
Sean Paterson <seanpaterson_25@msn.com>
2022-11-01 09:10:31 +0000

    feat: setup prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
-import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
-import './App.css';
+import "./App.css";
+
+import React from "react";
+
+import { Counter } from "./features/counter/Counter";
+import logo from "./logo.svg";
 
 function App() {
   return (
